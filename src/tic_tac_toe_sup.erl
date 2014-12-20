a8764ae06f2e898e76c5f8a80cbbf0d5b033b78e
Graham Hay <grahamrhay@gmail.com>
2014-12-20 14:56:15 +0000

    Tabs to spaces

--- src/tic_tac_toe_sup.erl
+++ src/tic_tac_toe_sup.erl
@@ -5,8 +5,8 @@
 -export([init/1]).
 
 start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [{t3_session_manager, {t3_session_manager, start_link, []}, permanent, 5000, worker, [t3_session_manager]}],
-	{ok, {{one_for_one, 1, 5}, Procs}}.
+    Procs = [{t3_session_manager, {t3_session_manager, start_link, []}, permanent, 5000, worker, [t3_session_manager]}],
+    {ok, {{one_for_one, 1, 5}, Procs}}.
