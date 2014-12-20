904b989fed913973226ff63262862ccd9a600f2f
Graham Hay <grahamrhay@gmail.com>
2014-12-20 13:18:00 +0000

    Create a session

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 PROJECT = tic_tac_toe
-DEPS = cowboy jiffy
+DEPS = cowboy jiffy uuid
 
 dep_cowboy = git https://github.com/ninenines/cowboy master
 dep_jiffy = git https://github.com/davisp/jiffy.git 0.13.3
+dep_uuid = git https://github.com/okeuday/uuid v1.3.3
 
 include erlang.mk
--- src/t3_connect_handler.erl
+++ src/t3_connect_handler.erl
@@ -7,6 +7,10 @@
 init(Req, Opts) ->
      {cowboy_websocket, Req, Opts}.
 
+websocket_handle({text, <<"new_session">>}, Req, State) ->
+    Resp = start_new_session(),
+    {reply, {text, Resp}, Req, State};
+
 websocket_handle({text, <<"new_game">>}, Req, State) ->
     Resp = start_new_game(),
     {reply, {text, Resp}, Req, State};
@@ -17,6 +21,10 @@ websocket_handle(_Frame, Req, State) ->
 websocket_info(_Info, Req, State) ->
     {ok, Req, State}.
 
+start_new_session() ->
+    SessionId = uuid:get_v4(),
+    jiffy:encode(#{type => <<"new_session">>, id => uuid:uuid_to_string(SessionId, binary_standard)}).
+
 start_new_game() ->
     GameId = 1,
-    jiffy:encode(#{type => <<"new_game">>, gameId => GameId}).
+    jiffy:encode(#{type => <<"new_game">>, id => GameId}).
