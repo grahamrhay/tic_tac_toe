70581a58c89a885a2868cbad91f47a7e28a870e5
Graham Hay <grahamrhay@gmail.com>
2014-12-24 14:54:16 +0000

    Split css & js into separate files

---
+++ priv/static/js/main.js
@@ -0,0 +1,67 @@
+var sessionId, gameId;
+
+var newGameBtn = document.getElementById('new-game');
+
+var socket = new WebSocket('ws://localhost:8080/connect');
+
+socket.onopen = function() {
+    send('new_session');
+};
+
+socket.onmessage = function(ev) {
+    console.log('Received data: ' + ev.data);
+    var msg = JSON.parse(ev.data);
+    if (msg.type === 'new_session') {
+        sessionId = msg.id;
+        newGameBtn.disabled = false;
+    } else if (msg.type === 'new_game') {
+        gameId = msg.id;
+        updateStatus('New game!');
+        clearBoard();
+    } else if (msg.type === 'your_turn') {
+        updateStatus('Your turn!');
+        enableBoard();
+    } else if (msg.type === 'wait') {
+        updateStatus('Waiting for other player...');
+    }
+};
+
+newGameBtn.onclick = function() {
+    var msg = JSON.stringify({type: 'new_game', sessionId: sessionId});
+    send(msg);
+    newGameBtn.disabled = true;
+    updateStatus('Waiting to join game...');
+};
+
+function clearBoard() {
+    for (var i = 1; i < 10; i++) {
+        document.getElementById('cell' + i).innerHTML = '';
+    }
+}
+
+function updateStatus(status) {
+    document.getElementById('status').innerHTML = status;
+}
+
+function send(msg) {
+    console.log('Sent msg: ' + msg);
+    socket.send(msg);
+}
+
+function enableBoard() {
+    for (var i = 1; i < 10; i++) {
+        document.getElementById('cell' + i).onclick = (function(id) {
+            return function() {
+                var msg = JSON.stringify({type: 'play', id: id})
+                send(msg)
+                disableBoard()
+            }
+        })(i)
+    }
+}
+
+function disableBoard() {
+    for (var i = 1; i < 10; i++) {
+        document.getElementById('cell' + i).onclick = null
+    }
+}
--- src/tic_tac_toe_app.erl
+++ src/tic_tac_toe_app.erl
@@ -7,8 +7,9 @@
 start(_Type, _Args) ->
     Dispatch = cowboy_router:compile([
         {'_', [
+               {"/connect", t3_connect_handler, []},
                {"/", cowboy_static, {priv_file, tic_tac_toe, "static/index.html"}},
-               {"/connect", t3_connect_handler, []}
+               {"/[...]", cowboy_static, {priv_dir, tic_tac_toe, "static/"}}
         ]}
     ]),
     cowboy:start_http(my_http_listener, 100, [{port, 8080}],
