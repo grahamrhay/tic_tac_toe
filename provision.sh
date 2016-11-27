a2ba80815bd5dce627f30f0633e5db5cd254ab37
Graham Hay <grahamrhay@gmail.com>
2016-11-27 12:16:53 +0000

    Install elm

--- provision.sh
+++ provision.sh
@@ -1,7 +1,20 @@
 sudo apt-get update
 sudo apt-get dist-upgrade -qy
 
+# install erlang
 echo "deb http://packages.erlang-solutions.com/debian jessie contrib" | sudo tee /etc/apt/sources.list.d/erlang.list
 wget -q -O- "http://packages.erlang-solutions.com/debian/erlang_solutions.asc" | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install erlang git build-essential -qy
+
+# install nodejs
+NODE_VERSION=v6.9.1
+curl -sSOL https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.gz
+mkdir /opt/nodejs
+tar -xzf node-$NODE_VERSION-linux-x64.tar.gz -C /opt/nodejs
+rm node-$NODE_VERSION-linux-x64.tar.gz
+ln -s /opt/nodejs/node-$NODE_VERSION-linux-x64/bin/node /usr/bin/node
+ln -s /opt/nodejs/node-$NODE_VERSION-linux-x64/bin/npm /usr/bin/npm
+
+# install elm
+npm install --global elm
